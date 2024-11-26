c7d0cdd512e55f1abe97a72ef65add9bda91cc09
Samyrrrrrr990 <samyar.shafiee2009@gmail.com>
2024-11-26 09:50:39 -0800

    Update limbo.js

--- limbo.js
+++ limbo.js
@@ -1,7 +1,1 @@
-function playLimbo(multiplier) {
-    const result = Math.random() * 100;
-    if (result < multiplier) {
-        return { win: true, payout: result * multiplier };
-    }
-    return { win: false, payout: 0 };
-}
+
